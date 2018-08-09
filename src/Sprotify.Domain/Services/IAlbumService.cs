041e3eed21e9bef006d10bd0ca94d1f46283c6ef
Wesley Cabus <wesley.cabus@realdolmen.com>
2018-08-09 14:56:48 +0200

    Added album service and bootstrap

---
+++ src/Sprotify.Domain/Services/IAlbumService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sprotify.Domain.Services
+{
+    public interface IAlbumService
+    {
+        Task<List<Album>> GetAlbums(bool includeSongs);
+        Task<Album> GetAlbumById(Guid id, bool includeSongs);
+        Task<Album> CreateAlbum(string title, string imageUri, string remarks);
+        Task<Album> CreateAlbum(string title, string imageUri, string remarks, Artist artist);
+        Task UpdateAlbum(Album album);
+        Task DeleteAlbum(Album album);
+    }
+}
